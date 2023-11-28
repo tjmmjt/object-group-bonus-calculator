ffcef124a90ad31db6cd2f378910f65e9a8c7570
Tucker Melssen <tuckerjmelssen@gmail.com>
2023-11-28 14:32:45 -0600

    Completed and Tested up through Individual Bonus Rules

--- client.js
+++ client.js
@@ -43,15 +43,64 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+for(let person of employees) {
+  console.log('Current person is:', person); // LOOP OVER EMPLOYEES  
+}
 
 
-
-// This function will calculate 1 employee's bonus!
+// This function will calculate 1 employee's bonus! ATTICUS
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
+  let employeeBonus = {
+    name: '',
+    bonusPercentage: '',
+    totalCompensation: '',
+    totalBonus: ''
+  }
+
+  employeeBonus.name = employee.name
+  // If employee gets a bonus
+  if (employee.reviewRating <= 2) { // 0% bonus, checking review rating
+    console.log("Doesn't receive a bonus");
+    employeeBonus.bonusPercentage = 0;
+  } else if (employee.reviewRating === 3) { // 4% of base annual income, checking review rating
+    employeeBonus.bonusPercentage = 0.04
+    console.log('4% bonus');
+    } else if (employee.reviewRating === 4) { // 6%, checking review rating
+      employeeBonus.bonusPercentage = 0.06;
+      console.log('6% bonus');
+    } else if (employee.reviewRating === 5) { // 10% bonus
+      employeeBonus.bonusPercentage = 0.1;
+      console.log('10% bonus');
+    } 
+
+    if(employee.employeeNumber.length === 4) {  // Senior Employee add 5%
+      employeeBonus.bonusPercentage += .05;
+      console.log('Senior Employee bonus Percentage:', employeeBonus.bonusPercentage); 
+    }
   
+    if(employee.annualSalary > 65000) { // Minus 1% for employees making over 65k
+      employeeBonus.bonusPercentage -= .01;
+      console.log('You make too much money. Minus 1% on your bonus!:', employeeBonus.bonusPercentage);
+    }
+
+    if(employeeBonus.bonusPercentage > .13) { // Capping bonuses at 13%
+      employeeBonus.bonusPercentage = .13
+    }
+
+    if(employeeBonus.bonusPercentage < 0) { // Bonuses can't be negative
+      employeeBonus.bonusPercentage = 0
+    }
+
+  console.log('employee bonus:', employeeBonus); // Logging Employee Bonus
+
+  return employeeBonus
+
   
   // return new object with bonus results
 
 }
+
+// Test
+calculateIndividualEmployeeBonus( employees[3] )
